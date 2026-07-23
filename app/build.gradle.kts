f05650db06ef81e3f5c0ec5c6e0bdf5a8fb74a3e
AtlasSurfin <federicocommisso98@gmail.com>
2026-07-24 00:34:18 +0200

    Currently reviewing LEARN tab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("cn.6tail:lunar:1.7.7")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.ai.client.generativeai:generativeai:0.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
